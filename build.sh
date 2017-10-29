943fb052c3467898ce9c77a8695abd06cc2732c5
Aurelio Jargas <verde@aurelio.net>
2017-10-29 19:51:48 -0200

    Simplify build script
    - `xargs` already avoids the need for a loop
    - No need to set `-t html`, all the files have the target configured

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
-#! /bin/bash
+#!/bin/bash
 
 git clone https://github.com/txt2tags/doc
 
-for file in `find . -name "*.t2t" | xargs`; do
-    txt2tags -t html $file
-done
+find . -name '*.t2t' | xargs txt2tags
