613e708261f5cdb5abdc7ebbd9a8781599292e45
Aurelio Jargas <verde@aurelio.net>
2017-10-31 12:03:52 -0200

    Skip conversion of inc and doc folders

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# Convert all t2t files
-find . -name '*.t2t' | xargs -n 1 txt2tags
-
 # Import files from the doc repo
 # Note: those files do not need conversion
 git clone https://github.com/txt2tags/doc
+
+# Convert all t2t files, except from inc and doc folders
+find . -name '*.t2t' \
+  -and -not -path './inc/*' \
+  -and -not -path './doc/*' \
+  -exec txt2tags {} \;
