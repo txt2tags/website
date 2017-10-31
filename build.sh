8353620e779ff48e2561595a5eb01a3ac6dbd357
Florent Gallaire <fgallaire@gmail.com>
2017-10-31 00:29:28 +0000

    Make build works again

--- build.sh
+++ build.sh
@@ -2,4 +2,12 @@
 
 git clone https://github.com/txt2tags/doc
 
-find . -name '*.t2t' | xargs -n 1 txt2tags
+# Should be:
+# find . -name '*.t2t' | xargs -n 1 txt2tags
+# But:
+# 1) some files in doc don't have target configured
+# 2) some files in doc fail to build, the loop seems to protect the global build
+
+for file in `find . -name "*.t2t" | xargs`; do
+    txt2tags -t html $file
+done
