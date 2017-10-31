e6976076397a89860f6f91e6cfd7d7c92e9c0903
Aurelio Jargas <verde@aurelio.net>
2017-10-31 11:31:25 -0200

    Clone doc repo after conversion
    So txt2tags won't try to convert the already converted doc files.

--- build.sh
+++ build.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
+# Convert all t2t files
+find . -name '*.t2t' | xargs -n 1 txt2tags
+
+# Import files from the doc repo
+# Note: those files do not need conversion
 git clone https://github.com/txt2tags/doc
-
-# Should be:
-# find . -name '*.t2t' | xargs -n 1 txt2tags
-# But:
-# 1) some files in doc don't have target configured
-# 2) some files in doc fail to build, the loop seems to protect the global build
-
-for file in `find . -name "*.t2t" | xargs`; do
-    txt2tags -t html $file
-done
