063b9d3dd44ec4918de594eb666851ee2f571f4d
Florent Gallaire <fgallaire@gmail.com>
2017-11-02 00:23:39 +0000

    Using doc-old repo until complete doc build

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # Import files from the doc repo
 # Note: those files do not need conversion
-git clone https://github.com/txt2tags/doc
+git clone https://github.com/txt2tags/doc-old doc
 
 # Convert all t2t files, except from inc and doc folders
 find . -name '*.t2t' \
